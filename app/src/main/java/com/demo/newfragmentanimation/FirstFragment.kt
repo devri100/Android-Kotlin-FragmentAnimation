caca616c158322b99fcbcb2c599ac5a84548595d
devri100 <k.ricsi28@gmail.com>
2020-05-17 14:25:03 +0200

    Nothing

---
+++ app/src/main/java/com/demo/newfragmentanimation/FirstFragment.kt
@@ -0,0 +1,33 @@
+package com.demo.newfragmentanimation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_new.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class FirstFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        button_add.setOnClickListener {
+            requireFragmentManager().beginTransaction()
+                .setCustomAnimations(R.anim.slide_in_bottom, R.anim.slide_out_bottom, R.anim.slide_in_bottom, R.anim.slide_out_bottom)
+                .replace(R.id.container, NewFragment())
+                .addToBackStack(null)
+                .commit()
+        }
+    }
+
+}
---
+++ app/src/main/java/com/demo/newfragmentanimation/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.demo.newfragmentanimation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if(savedInstanceState == null){
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, FirstFragment())
+                    .commit()
+        }
+    }
+}
